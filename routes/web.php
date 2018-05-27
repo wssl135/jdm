69fee5ea8d79ee3b40768f18e0e72c6d103401b3
lover <wssl135@163.com>
2018-05-28 00:02:24 +0800

    first ver

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/adminlte', function () {
+
+	return view('adminlte');
+});
